6a41befb8791a18039532a46410bde01b6c4a21c
0x5c3p73r <0x5c3p73r@gmail.com>
2022-10-31 11:38:15 +0800

    bump 0.3.0

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_view/railtie"
 Bundler.require(*Rails.groups)
 
 module Subway
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
